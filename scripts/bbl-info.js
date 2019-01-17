5c224547da5085a49c1b61778ff948848e027822
Chris Whong <chris@whong.org>
2019-01-16 22:17:22 -0500

    add slack adapter

--- scripts/bbl-info.js
+++ scripts/bbl-info.js
@@ -7,7 +7,7 @@ const bblRegex = /[1-5]{1}[0-9]{5}[0-9]{4}/;
 module.exports = (robot) => {
   robot.hear(bblRegex, async (res) => {
     const [ bbl ] = res.message.text.match(bblRegex);
-    const apiCall = `https://planninglabs.carto.com/api/v2/sql?q=SELECT address, zonedist1, borocode FROM mappluto_18v_1_1 WHERE bbl = ${bbl}`;
+    const apiCall = `https://planninglabs.carto.com/api/v2/sql?q=SELECT address, zonedist1, borocode, block, lot FROM mappluto_18v_1_1 WHERE bbl = ${bbl}`;
 
     try {
       const { rows } = await fetch(apiCall)
@@ -18,10 +18,12 @@ module.exports = (robot) => {
         const {
           address,
           borocode,
+          block,
+          lot,
           zonedist1,
         } = row;
 
-        res.send(`Hi! BBL ${bbl} is known as ${address} in ${boroughArray[borocode]}.  It's located in a ${zonedist1} district.`);
+        res.send(`Hi! BBL ${bbl} is known as ${address} in ${boroughArray[borocode]}.  It's located in a ${zonedist1} district. <https://zola.planning.nyc.gov/lot/${borocode}/${block}/${lot}|View in ZoLa>`);
       } else {
         res.send(`Hi! ${bbl} looks like a BBL, but it doesn't exist in PLUTO.`)
       }
