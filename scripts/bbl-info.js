48d630778b772d10455022589f41d0422827a455
Chris Whong <chris@whong.org>
2019-03-07 17:02:06 -0500

    fix bbl responder

--- scripts/bbl-info.js
+++ scripts/bbl-info.js
@@ -4,34 +4,38 @@ const fetch = require('node-fetch');
 
 const boroughArray = [null, 'Manhattan', 'The Bronx', 'Brooklyn', 'Queens', 'Staten Island'];
 
-const bblRegex = /^bbl \b[1-5]{1}[0-9]{5}[0-9]{4}\b/;
-
 module.exports = (robot) => {
-  robot.respond(bblRegex, async (res) => {
-    const [ bbl ] = res.message.text.match(bblRegex);
-    const apiCall = `https://planninglabs.carto.com/api/v2/sql?q=SELECT address, zonedist1, borocode, block, lot FROM mappluto_18v_1_1 WHERE bbl = ${bbl}`;
+  robot.respond(/bbl/, async (res) => {
+    const bblRegex = /\b[1-5]{1}[0-9]{5}[0-9]{4}\b/
+    const match = res.message.text.match(bblRegex)
+    if (match === null) {
+      res.send('That doesn\'t look like a BBL to me.');
+    } else {
+      try {
+        const [ bbl ] = match;
+        const apiCall = `https://planninglabs.carto.com/api/v2/sql?q=SELECT address, zonedist1, borocode, block, lot FROM mappluto WHERE bbl = ${bbl}`;
 
-    try {
-      const { rows } = await fetch(apiCall)
-        .then(d => d.json());
-      const [ row ] = rows;
+        const { rows } = await fetch(apiCall)
+          .then(d => d.json());
+        const [ row ] = rows;
 
-      if (row) {
-        const {
-          address,
-          borocode,
-          block,
-          lot,
-          zonedist1,
-        } = row;
+        if (row) {
+          const {
+            address,
+            borocode,
+            block,
+            lot,
+            zonedist1,
+          } = row;
 
-        res.send(`Hi! BBL ${bbl} is known as ${address} in ${boroughArray[borocode]}.  It's located in an ${zonedist1} district. <https://zola.planning.nyc.gov/lot/${borocode}/${block}/${lot}|View in ZoLa>`);
-      } else {
-        res.send(`Hi! ${bbl} looks like a BBL, but it doesn't exist in PLUTO.`)
+          res.send(`Hi! BBL ${bbl} is known as ${address} in ${boroughArray[borocode]}.  It's located in an ${zonedist1} district. <https://zola.planning.nyc.gov/lot/${borocode}/${block}/${lot}|View in ZoLa>`);
+        } else {
+          res.send(`Hi! ${bbl} looks like a BBL, but it doesn't exist in PLUTO.`)
+        }
+
+      } catch (e) {
+        console.log(e)
       }
-
-    } catch (e) {
-      console.log(e)
     }
   });
 }
