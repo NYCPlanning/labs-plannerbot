194d5c3b390156b9a26eb55fdd845cd4f4194235
Jonathan Pichot <jonathan@pichot.us>
2019-05-17 15:19:27 -0400

    Reference NYCPlanning repo for benefits

--- scripts/benefits.js
+++ scripts/benefits.js
@@ -1,9 +1,9 @@
 module.exports = (robot) => {
   robot.hear(/dentists*/i, async (res) => {
-    res.send("🦷 Looking for DC37 dentists? Here's a <https://github.com/pichot/nyc-dc37-benefits-map/blob/master/dc37_dentists.geojson|map> and <https://github.com/pichot/nyc-dc37-benefits-map/blob/master/dc37_dental.csv|list> of eligible providers.");
+    res.send("🦷 Looking for DC37 dentists? Here's a <https://github.com/NYCPlanning/nyc-dc37-benefits-map/blob/master/dc37_dentists.geojson|map> and <https://github.com/NYCPlanning/nyc-dc37-benefits-map/blob/master/dc37_dental.csv|list> of eligible providers.");
   })
 
   robot.hear(/optical|vision|opticians*|opthamologists*/i, async (res) => {
-    res.send("👓 Looking for DC37 opthamologists? Here's a <https://github.com/pichot/nyc-dc37-benefits-map/blob/master/dc37_optical.geojson|map> and <https://github.com/pichot/nyc-dc37-benefits-map/blob/master/dc37_optical.csv|list> of eligible providers.");
+    res.send("👓 Looking for DC37 opthamologists? Here's a <https://github.com/NYCPlanning/nyc-dc37-benefits-map/blob/master/dc37_optical.geojson|map> and <https://github.com/NYCPlanning/nyc-dc37-benefits-map/blob/master/dc37_optical.csv|list> of eligible providers.");
   })
 }
