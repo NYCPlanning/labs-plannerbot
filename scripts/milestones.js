37855ed18d5b02dc15f85a00eb6df156e5442ca6
Amina Dieng <adieng@DCP-APPLE-1786.local>
2019-06-26 13:55:43 -0400

    successfully consume webhook and run script

--- scripts/milestones.js
+++ scripts/milestones.js
@@ -15,35 +15,35 @@ function defineOptions(repoName) {
   return options;
 }
 
-function runScript(repo_name, options, response) {
+function runScript(repo_name, options) {
   PythonShell.run('set-milestones.py', options, function (err, results) {
     if (err) {
       // results is an array consisting of messages collected during execution
       throw err;
-      response.send('Something went wrong!');
+      console.log('Something went wrong!');
     } else {
-      response.send('Successfully created sprint milestones for ' + repo_name);
+      console.log('Successfully created sprint milestones for ' + repo_name);
     }
   });
 }
 
 module.exports = (robot) => {
 
-  // consume webhook
-  @robot.on "github-repo-event", (repoEvent) => {
+  // consume github webhook
+  robot.on("github-repo-event", (repoEvent) => {
     switch (repoEvent.eventType) {
       case "push":
-        let payload = repoEvent.payload;
-        let repo = payload['repository']['name'];
+        let githubPayload = JSON.parse(repoEvent.payload.payload);
+        let repo = githubPayload.repository.name;
         let options = defineOptions(repo);
         runScript(repo, options);
     }
-  }
+  });
 
   // listen for command prompt
   robot.respond( /milestones (.*)/i, async (res) => {
     let repoName = res.match[1];
     let options = defineOptions(repo);
-    runScript(repo, options, res);
+    runScript(repo, options);
   });
 }
