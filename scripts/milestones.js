4858d03ad0c92b819889b6418101565e9fca5e59
Amina Dieng <adieng@DCP-APPLE-1786.local>
2019-06-26 10:54:55 -0400

    emit and consume git webhook event

--- scripts/milestones.js
+++ scripts/milestones.js
@@ -15,35 +15,51 @@ function defineOptions(repoName) {
   return options;
 }
 
-function runScript(repo_name, options) {
+function runScript(repo_name, options, response) {
   PythonShell.run('set-milestones.py', options, function (err, results) {
     if (err) {
       // results is an array consisting of messages collected during execution
       throw err;
-      console.log('Something went wrong!');
+      response.send('Something went wrong!');
     } else {
-      console.log('Successfully created sprint milestones for ' + repo_name);
+      response.send('Successfully created sprint milestones for ' + repo_name);
     }
   });
 }
 
 module.exports = (robot) => {
 
-  // listen for github webhook
-  robot.on("github-repo-event", (repoEvent) => {
-    switch (repoEvent.eventType) {
-      case "push":
-        let payload = repoEvent.payload
-        let repo = payload.repository.name
-        let options = defineOptions(repo);
-        runScript(repo, options);
-    }
-  });
+  // emit hubot event upon incoming webhook message
+  robot.router.post "/hubot/github-repo-listener", (req, res) => {
+    let eventBody = {
+      eventType   : req.headers["x-github-event"]
+      signature   : req.headers["X-Hub-Signature"]
+      deliveryId  : req.headers["X-Github-Delivery"]
+      payload     : req.body
+      query       : querystring.parse(url.parse(req.url).query)
+    };
+
+    robot.emit "github-repo-event", eventBody;
+  }
+
+  // consume webhook
+  robot.on "github-repo-event", (repoEvent) => {
+    robot.send('Webhook has been consumed!')
+
+    // switch (repoEvent.eventType) {
+    //   case "push":
+    //     let payload = repoEvent.payload;
+    //     let repo = payload.repository.name;
+    //     let options = defineOptions(repo);
+    //     console.log("here in webhook consumer")
+    //     //runScript(repo, options);
+    // }
+  }
 
   // listen for command prompt
   robot.respond( /milestones (.*)/i, async (res) => {
     let repoName = res.match[1];
     let options = defineOptions(repo);
-    runScript(repo, options);
+    runScript(repo, options, res);
   });
 }
